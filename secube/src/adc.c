dff5554ac2e97d512b680c7a28ca0c152d383af7
admin00 <admin00@localhost>
2018-04-20 19:01:55 +0200

    adc.c modificato

--- secube/src/adc.c
+++ secube/src/adc.c
@@ -30,7 +30,7 @@
   * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
-  *///ciaoooooooooooooo
+  *///ciaoooooooooooooosadasdasjdaskljdaskljsd
 
 /* Includes ------------------------------------------------------------------*/
 #include "adc.h"
